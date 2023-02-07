f4598c0c0770c9d658d7e971cc42406897454a98
Pranav Waghmare <piyushw55@gmail.com>
2023-02-07 16:42:30 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake.

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -10,18 +10,34 @@
 
             Console.WriteLine("\n-----------------------------------------------------------\n");
 
-            int pos = 0;  // Position
-            int dice = 0; // Dice
+            int pos = 0;       // Position Initializin
+            int dice = 0;      // Dice 
+            int opt = 0;       // options
 
-           
 
-            //Position Initializing
+            Random random = new Random();       //Creating object random
+            dice = random.Next(1, 7);       // Using random method 
+            Console.WriteLine("The postition of player starts: {0} and random number of Dice: {1}\n", pos, dice);
 
-            Random random = new Random();
 
-            dice = random.Next(1, 7);
+            Random random1 = new Random();      //Creating object random
+            opt = random1.Next(1, 4);        //Using random method
 
-            Console.WriteLine("The position of player starts: {0} and random number of Dice: {1}", pos, dice);
+            switch (opt)         //Checking for options using switch case
+            {
+                case 1:
+                    pos = pos + dice;
+                    Console.WriteLine("The player moves ahead by number Dice value : " + pos);
+                    break;
+                case 2:
+                    pos = pos - dice;
+                    Console.WriteLine("The player moves behind by number Dice value : " + pos);
+                    break;
+                default:
+                    Console.WriteLine("The player stay at same place : " + pos);
+                    break;
+            }
+            
 
         }
     }
