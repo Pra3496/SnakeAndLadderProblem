ced38ddc264c9e645e78c3d7e0c6cb7d5e4a152e
Pranav Waghmare <piyushw55@gmail.com>
2023-02-07 16:32:47 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n----------------------------START--------------------------\n");
+
+            Console.WriteLine("\tWelcome To Snake and Ladder Game Application");
+
+            Console.WriteLine("\n-----------------------------------------------------------\n");
+
+            int pos = 0; // position
+
+
+            //Position Initializing
+
+            Console.WriteLine("The position of player starts: " + pos);
+
+        }
+    }
+}
+
+
