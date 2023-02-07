a236549b9c3413a6f7be955555d25606fc225d11
Pranav Waghmare <piyushw55@gmail.com>
2023-02-07 16:37:54 +0530

    The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -10,12 +10,18 @@
 
             Console.WriteLine("\n-----------------------------------------------------------\n");
 
-            int pos = 0; // position
+            int pos = 0;  // Position
+            int dice = 0; // Dice
 
+           
 
             //Position Initializing
 
-            Console.WriteLine("The position of player starts: " + pos);
+            Random random = new Random();
+
+            dice = random.Next(1, 7);
+
+            Console.WriteLine("The position of player starts: {0} and random number of Dice: {1}", pos, dice);
 
         }
     }
