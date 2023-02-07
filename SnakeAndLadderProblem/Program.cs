531207db45ec6ea74de22218cd70c041049c7545
Pranav Waghmare <piyushw55@gmail.com>
2023-02-07 17:15:42 +0530

    Report the number of times the dice was played to win the game and also the position after every die role

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -13,8 +13,10 @@
             int pos = 0;       // Position Initializin
             int dice = 0;      // Dice 
             int opt = 0;       // options
+            int winCnt = 0;
 
 
+            Console.WriteLine("_______| Welcome! to Snake and Ladder Game |_______\n");
 
             Random random = new Random();       //Creating object random
 
@@ -27,6 +29,7 @@
             {
                 dice = random.Next(1, 7); // Using random method 
                 opt = random1.Next(1, 4); //Using random method
+                winCnt++;
 
                 switch (opt)         //Checking for options using switch case
                 {
@@ -52,10 +55,17 @@
                         break;
                 }
 
-
-
             }
+
+            Console.WriteLine("\n\n\n----------------------------END----------------------------\n");
+            Console.WriteLine("\t{0} times the dice was played to win the game ", winCnt);
+            Console.WriteLine("-----------------------------------------------------------\n");
+
+
+
+
         }
+    }
 }
 
 
